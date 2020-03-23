98d6e2d10a0535436b9b02ea4050e64b777854a4
Leyhara <vocalold@naver.com>
2020-03-23 11:44:17 +0900

    0.423

--- 17.Kiana/Kiana_Skill_A.py
+++ 17.Kiana/Kiana_Skill_A.py
@@ -1,192 +1,290 @@
 
-x = 64;
-y = 64;
+x = 96;
+y = 32;
 
 Trigger { -- Skill : S
    players = {Force1, Force2},
    conditions = {
-      Deaths(CurrentPlayer, Exactly, 2000, " * Devouring One");
-      Deaths(CurrentPlayer, Exactly, 300, " `SkillStep");
-      Bring(CurrentPlayer, AtLeast, 1, " * Devouring One", "Anywhere");
-      Deaths(CurrentPlayer, Exactly, 1, " `SkillCount");
-      Deaths(CurrentPlayer, Exactly, i, " `SkillLoop");
+      Deaths(CurrentPlayer, Exactly, 4000, 77);
+      Deaths(CurrentPlayer, Exactly, 100, " `SkillStep");
+      Bring(CurrentPlayer, AtLeast, 1, 77, "Anywhere");
+      Deaths(CurrentPlayer, Exactly, 0, " `SkillCount");
+      Deaths(CurrentPlayer, Exactly, 0, " `SkillLoop");
       Deaths(CurrentPlayer, Exactly, 0, " `SkillWait");
    },
    actions = {
       Comment("Skill : S");
       PreserveTrigger();
-      CreateUnit(2, "60 + 1n Danimoth", "[Skill]Unit_Wait_8", CurrentPlayer);
-      CreateUnit(2, "60 + 1n Archon", "[Skill]Unit_Wait_8", CurrentPlayer);
-      SetInvincibility(Enable, "Any unit", CurrentPlayer, "[Skill]Unit_Wait_ALL");
-      MoveLocation("15.Rin", " * Devouring One", CurrentPlayer, "Anywhere");
-      LMove(165, x, y);
-      MoveUnit(1, "60 + 1n Danimoth", CurrentPlayer, "[Skill]Unit_Wait_ALL", "15.Rin");
-      MoveUnit(1, "60 + 1n Archon", CurrentPlayer, "[Skill]Unit_Wait_ALL", "15.Rin");
-      MoveLocation("15.Rin", " * Devouring One", CurrentPlayer, "Anywhere");
-      LMove(165, -x, -y);
-      MoveUnit(1, "60 + 1n Danimoth", CurrentPlayer, "[Skill]Unit_Wait_ALL", "15.Rin");
-      MoveUnit(1, "60 + 1n Archon", CurrentPlayer, "[Skill]Unit_Wait_ALL", "15.Rin");
+      CreateUnit(2, "40 + 1n Wraith", "[Skill]Unit_Wait_1", CurrentPlayer);
+      CreateUnit(2, "60 + 1n Archon", "[Skill]Unit_Wait_1", CurrentPlayer);
+      SetInvincibility(Enable, "Any unit", CurrentPlayer, 40);
+      MoveLocation(169, 77, CurrentPlayer, "Anywhere");
+      LMove(169, x, y);
+      MoveUnit(1, "40 + 1n Wraith", CurrentPlayer, 40, 169);
+      MoveUnit(1, "60 + 1n Archon", CurrentPlayer, 40, 169);
+      MoveLocation(169, 77, CurrentPlayer, "Anywhere");
+      LMove(169, -x, -y);
+      MoveUnit(1, "40 + 1n Wraith", CurrentPlayer, 40, 169);
+      MoveUnit(1, "60 + 1n Archon", CurrentPlayer, 40, 169);
       KillUnitAt(All, "60 + 1n Archon", "Anywhere", CurrentPlayer);
-      MoveLocation("15.Rin", " * Devouring One", CurrentPlayer, "Anywhere");
-      Order("60 + 1n Danimoth", CurrentPlayer, "Anywhere", Attack, "15.Rin");
+      MoveLocation(169, 77, CurrentPlayer, "Anywhere");
+      Order("40 + 1n Wraith", CurrentPlayer, "Anywhere", Attack, 169);
       Wait(0);
-      RemoveUnitAt(All, "60 + 1n Danimoth", "Anywhere", CurrentPlayer);
+      RemoveUnitAt(All, "40 + 1n Wraith", "Anywhere", CurrentPlayer);
       SetDeaths(CurrentPlayer, Add, 1, " `SkillLoop");
    },
 }
-end
 
-radius1 = 1 / 4 * math.pi;
-radius2 = 2 / 4 * math.pi;
-
-d = 50;
-
-x1 = math.sin(radius1) * d;
-y1 = math.cos(radius1) * d;
-x1 = math.floor(x1 + 0.5);
-y1 = math.floor(y1 + 0.5);
-
-x2 = math.sin(radius2) * d;
-y2 = math.cos(radius2) * d;
-x2 = math.floor(x2 + 0.5);
-y2 = math.floor(y2 + 0.5);
+x = 32;
+y = 32;
 
 Trigger { -- Skill : S
    players = {Force1, Force2},
    conditions = {
-      Deaths(CurrentPlayer, Exactly, 2000, " * Devouring One");
-      Deaths(CurrentPlayer, Exactly, 300, " `SkillStep");
-      Bring(CurrentPlayer, AtLeast, 1, " * Devouring One", "Anywhere");
-      Deaths(CurrentPlayer, Exactly, 1, " `SkillCount");
-      Deaths(CurrentPlayer, Exactly, 8, " `SkillLoop");
+      Deaths(CurrentPlayer, Exactly, 4000, 77);
+      Deaths(CurrentPlayer, Exactly, 100, " `SkillStep");
+      Bring(CurrentPlayer, AtLeast, 1, 77, "Anywhere");
+      Deaths(CurrentPlayer, Exactly, 0, " `SkillCount");
+      Deaths(CurrentPlayer, Exactly, 1, " `SkillLoop");
       Deaths(CurrentPlayer, Exactly, 0, " `SkillWait");
    },
    actions = {
       Comment("Skill : S");
       PreserveTrigger();
-      CreateUnitWithProperties(8, "60 + 1n Danimoth", "[Skill]Unit_Wait_8", CurrentPlayer, {
-         clocked = false,
-         burrowed = false,
-         intransit = false,
-         hallucinated = false,
-         invincible = false,
-         hitpoint = 100,
-         shield = 100,
-         energy = 100,
-         resource = 0,
-         hanger = 0,
-      });  
-      SetInvincibility(Enable, "Any unit", CurrentPlayer, "[Skill]Unit_Wait_ALL");
-      MoveLocation("15.Rin", " * Devouring One", CurrentPlayer, "Anywhere");
-      LMove(165, x1, y1);
-      MoveUnit(1, "60 + 1n Danimoth", CurrentPlayer, "[Skill]Unit_Wait_ALL", "15.Rin");
-      MoveLocation("15.Rin", " * Devouring One", CurrentPlayer, "Anywhere");
-      LMove(165, -x1, -y1);
-      MoveUnit(1, "60 + 1n Danimoth", CurrentPlayer, "[Skill]Unit_Wait_ALL", "15.Rin");
-      MoveLocation("15.Rin", " * Devouring One", CurrentPlayer, "Anywhere");
-      LMove(165, y1, -x1);
-      MoveUnit(1, "60 + 1n Danimoth", CurrentPlayer, "[Skill]Unit_Wait_ALL", "15.Rin");
-      MoveLocation("15.Rin", " * Devouring One", CurrentPlayer, "Anywhere");
-      LMove(165, -y1, x1);
-      MoveUnit(1, "60 + 1n Danimoth", CurrentPlayer, "[Skill]Unit_Wait_ALL", "15.Rin");
-      MoveLocation("15.Rin", " * Devouring One", CurrentPlayer, "Anywhere");
-      LMove(165, x2, y2);
-      MoveUnit(1, "60 + 1n Danimoth", CurrentPlayer, "[Skill]Unit_Wait_ALL", "15.Rin");
-      MoveLocation("15.Rin", " * Devouring One", CurrentPlayer, "Anywhere");
-      LMove(165, -x2, -y2);
-      MoveUnit(1, "60 + 1n Danimoth", CurrentPlayer, "[Skill]Unit_Wait_ALL", "15.Rin");
-      MoveLocation("15.Rin", " * Devouring One", CurrentPlayer, "Anywhere");
-      LMove(165, y2, -x2);
-      MoveUnit(1, "60 + 1n Danimoth", CurrentPlayer, "[Skill]Unit_Wait_ALL", "15.Rin");
-      MoveLocation("15.Rin", " * Devouring One", CurrentPlayer, "Anywhere");
-      LMove(165, -y2, x2);
-      MoveUnit(1, "60 + 1n Danimoth", CurrentPlayer, "[Skill]Unit_Wait_ALL", "15.Rin");
-      MoveLocation("15.Rin", " * Devouring One", CurrentPlayer, "Anywhere");
-      MoveUnit(All, "60 + 1n Danimoth", CurrentPlayer, "[Skill]Unit_Wait_ALL", "15.Rin");
-      Order("60 + 1n Danimoth", CurrentPlayer, "Anywhere", Attack, "15.Rin");
-      Wait(120);
-      KillUnitAt(All, "60 + 1n Danimoth", "Anywhere", CurrentPlayer);
+      CreateUnit(2, "40 + 1n Wraith", "[Skill]Unit_Wait_1", CurrentPlayer);
+      CreateUnit(2, "60 + 1n Archon", "[Skill]Unit_Wait_1", CurrentPlayer);
+      SetInvincibility(Enable, "Any unit", CurrentPlayer, 40);
+      MoveLocation(169, 77, CurrentPlayer, "Anywhere");
+      LMove(169, x, y);
+      MoveUnit(1, "40 + 1n Wraith", CurrentPlayer, 40, 169);
+      MoveUnit(1, "60 + 1n Archon", CurrentPlayer, 40, 169);
+      MoveLocation(169, 77, CurrentPlayer, "Anywhere");
+      LMove(169, -x, -y);
+      MoveUnit(1, "40 + 1n Wraith", CurrentPlayer, 40, 169);
+      MoveUnit(1, "60 + 1n Archon", CurrentPlayer, 40, 169);
+      KillUnitAt(All, "60 + 1n Archon", "Anywhere", CurrentPlayer);
+      MoveLocation(169, 77, CurrentPlayer, "Anywhere");
+      Order("40 + 1n Wraith", CurrentPlayer, "Anywhere", Attack, 169);
       Wait(0);
+      RemoveUnitAt(All, "40 + 1n Wraith", "Anywhere", CurrentPlayer);
       SetDeaths(CurrentPlayer, Add, 1, " `SkillLoop");
    },
 }
 
+x = -32;
+y = 32;
+
 Trigger { -- Skill : S
    players = {Force1, Force2},
    conditions = {
-      Deaths(CurrentPlayer, Exactly, 2000, " * Devouring One");
-      Deaths(CurrentPlayer, Exactly, 300, " `SkillStep");
-      Bring(CurrentPlayer, AtLeast, 1, " * Devouring One", "Anywhere");
-      Deaths(CurrentPlayer, Exactly, 1, " `SkillCount");
-      Deaths(CurrentPlayer, Exactly, 9, " `SkillLoop");
+      Deaths(CurrentPlayer, Exactly, 4000, 77);
+      Deaths(CurrentPlayer, Exactly, 100, " `SkillStep");
+      Bring(CurrentPlayer, AtLeast, 1, 77, "Anywhere");
+      Deaths(CurrentPlayer, Exactly, 0, " `SkillCount");
+      Deaths(CurrentPlayer, Exactly, 2, " `SkillLoop");
       Deaths(CurrentPlayer, Exactly, 0, " `SkillWait");
    },
    actions = {
       Comment("Skill : S");
       PreserveTrigger();
-      CreateUnitWithProperties(8, "40 + 1n Mojo", "[Skill]Unit_Wait_8", CurrentPlayer, {
-         clocked = false,
-         burrowed = false,
-         intransit = false,
-         hallucinated = false,
-         invincible = false,
-         hitpoint = 100,
-         shield = 100,
-         energy = 100,
-         resource = 0,
-         hanger = 0,
-      });  
-      SetInvincibility(Enable, "Any unit", CurrentPlayer, "[Skill]Unit_Wait_ALL");
-      MoveLocation("15.Rin", " * Devouring One", CurrentPlayer, "Anywhere");
-      LMove(165, x1, y1);
-      MoveUnit(1, "40 + 1n Mojo", CurrentPlayer, "[Skill]Unit_Wait_ALL", "15.Rin");
-      MoveLocation("15.Rin", " * Devouring One", CurrentPlayer, "Anywhere");
-      LMove(165, -x1, -y1);
-      MoveUnit(1, "40 + 1n Mojo", CurrentPlayer, "[Skill]Unit_Wait_ALL", "15.Rin");
-      MoveLocation("15.Rin", " * Devouring One", CurrentPlayer, "Anywhere");
-      LMove(165, y1, -x1);
-      MoveUnit(1, "40 + 1n Mojo", CurrentPlayer, "[Skill]Unit_Wait_ALL", "15.Rin");
-      MoveLocation("15.Rin", " * Devouring One", CurrentPlayer, "Anywhere");
-      LMove(165, -y1, x1);
-      MoveUnit(1, "40 + 1n Mojo", CurrentPlayer, "[Skill]Unit_Wait_ALL", "15.Rin");
-      MoveLocation("15.Rin", " * Devouring One", CurrentPlayer, "Anywhere");
-      LMove(165, x2, y2);
-      MoveUnit(1, "40 + 1n Mojo", CurrentPlayer, "[Skill]Unit_Wait_ALL", "15.Rin");
-      MoveLocation("15.Rin", " * Devouring One", CurrentPlayer, "Anywhere");
-      LMove(165, -x2, -y2);
-      MoveUnit(1, "40 + 1n Mojo", CurrentPlayer, "[Skill]Unit_Wait_ALL", "15.Rin");
-      MoveLocation("15.Rin", " * Devouring One", CurrentPlayer, "Anywhere");
-      LMove(165, y2, -x2);
-      MoveUnit(1, "40 + 1n Mojo", CurrentPlayer, "[Skill]Unit_Wait_ALL", "15.Rin");
-      MoveLocation("15.Rin", " * Devouring One", CurrentPlayer, "Anywhere");
-      LMove(165, -y2, x2);
-      MoveUnit(1, "40 + 1n Mojo", CurrentPlayer, "[Skill]Unit_Wait_ALL", "15.Rin");
-      MoveLocation("15.Rin", " * Devouring One", CurrentPlayer, "Anywhere");
-      Order("40 + 1n Mojo", CurrentPlayer, "Anywhere", Attack, "15.Rin");
-      Wait(120);
-      KillUnitAt(All, "40 + 1n Mojo", "Anywhere", CurrentPlayer);
+      CreateUnit(2, "40 + 1n Wraith", "[Skill]Unit_Wait_1", CurrentPlayer);
+      CreateUnit(2, "60 + 1n Archon", "[Skill]Unit_Wait_1", CurrentPlayer);
+      SetInvincibility(Enable, "Any unit", CurrentPlayer, 40);
+      MoveLocation(169, 77, CurrentPlayer, "Anywhere");
+      LMove(169, x, y);
+      MoveUnit(1, "40 + 1n Wraith", CurrentPlayer, 40, 169);
+      MoveUnit(1, "60 + 1n Archon", CurrentPlayer, 40, 169);
+      MoveLocation(169, 77, CurrentPlayer, "Anywhere");
+      LMove(169, -x, -y);
+      MoveUnit(1, "40 + 1n Wraith", CurrentPlayer, 40, 169);
+      MoveUnit(1, "60 + 1n Archon", CurrentPlayer, 40, 169);
+      KillUnitAt(All, "60 + 1n Archon", "Anywhere", CurrentPlayer);
+      MoveLocation(169, 77, CurrentPlayer, "Anywhere");
+      Order("40 + 1n Wraith", CurrentPlayer, "Anywhere", Attack, 169);
       Wait(0);
+      RemoveUnitAt(All, "40 + 1n Wraith", "Anywhere", CurrentPlayer);
       SetDeaths(CurrentPlayer, Add, 1, " `SkillLoop");
    },
 }
 
+x = -96;
+y = 32;
 
 Trigger { -- Skill : S
    players = {Force1, Force2},
    conditions = {
-      Deaths(CurrentPlayer, Exactly, 2000, " * Devouring One");
-      Deaths(CurrentPlayer, Exactly, 300, " `SkillStep");
-      Bring(CurrentPlayer, AtLeast, 1, " * Devouring One", "Anywhere");
-      Deaths(CurrentPlayer, Exactly, 1, " `SkillCount");
-      Deaths(CurrentPlayer, Exactly, 10, " `SkillLoop");
+      Deaths(CurrentPlayer, Exactly, 4000, 77);
+      Deaths(CurrentPlayer, Exactly, 100, " `SkillStep");
+      Bring(CurrentPlayer, AtLeast, 1, 77, "Anywhere");
+      Deaths(CurrentPlayer, Exactly, 0, " `SkillCount");
+      Deaths(CurrentPlayer, Exactly, 3, " `SkillLoop");
       Deaths(CurrentPlayer, Exactly, 0, " `SkillWait");
    },
    actions = {
       Comment("Skill : S");
       PreserveTrigger();
-      Wait(500);
-      SetDeaths(CurrentPlayer, SetTo, 0, " `SkillCount");
+      CreateUnit(2, "40 + 1n Wraith", "[Skill]Unit_Wait_1", CurrentPlayer);
+      CreateUnit(2, "60 + 1n Archon", "[Skill]Unit_Wait_1", CurrentPlayer);
+      SetInvincibility(Enable, "Any unit", CurrentPlayer, 40);
+      MoveLocation(169, 77, CurrentPlayer, "Anywhere");
+      LMove(169, x, y);
+      MoveUnit(1, "40 + 1n Wraith", CurrentPlayer, 40, 169);
+      MoveUnit(1, "60 + 1n Archon", CurrentPlayer, 40, 169);
+      MoveLocation(169, 77, CurrentPlayer, "Anywhere");
+      LMove(169, -x, -y);
+      MoveUnit(1, "40 + 1n Wraith", CurrentPlayer, 40, 169);
+      MoveUnit(1, "60 + 1n Archon", CurrentPlayer, 40, 169);
+      KillUnitAt(All, "60 + 1n Archon", "Anywhere", CurrentPlayer);
+      MoveLocation(169, 77, CurrentPlayer, "Anywhere");
+      Order("40 + 1n Wraith", CurrentPlayer, "Anywhere", Attack, 169);
+      Wait(0);
+      RemoveUnitAt(All, "40 + 1n Wraith", "Anywhere", CurrentPlayer);
+	  Wait(0);
       SetDeaths(CurrentPlayer, SetTo, 0, " `SkillLoop");
-      SetDeaths(CurrentPlayer, Add, 12, " `SkillWait");
+      SetDeaths(CurrentPlayer, Add, 1, " `SkillCount");
    },
 }
+
+x = 96;
+y = 32;
+
+Trigger { -- Skill : S
+   players = {Force1, Force2},
+   conditions = {
+      Deaths(CurrentPlayer, Exactly, 4000, 77);
+      Deaths(CurrentPlayer, Exactly, 100, " `SkillStep");
+      Bring(CurrentPlayer, AtLeast, 1, 77, "Anywhere");
+      Deaths(CurrentPlayer, Exactly, 1, " `SkillCount");
+      Deaths(CurrentPlayer, Exactly, 0, " `SkillLoop");
+   },
+   actions = {
+      Comment("Skill : S");
+      PreserveTrigger();
+      CreateUnit(2, "40 + 1n Mojo", "[Skill]Unit_Wait_1", CurrentPlayer);
+      CreateUnit(2, "Protoss Dark Archon", "[Skill]Unit_Wait_1", CurrentPlayer);
+      SetInvincibility(Enable, "Any unit", CurrentPlayer, 40);
+      MoveLocation(169, 77, CurrentPlayer, "Anywhere");
+      LMove(169, x, y);
+      MoveUnit(1, "40 + 1n Mojo", CurrentPlayer, 40, 169);
+      MoveUnit(1, "Protoss Dark Archon", CurrentPlayer, 40, 169);
+      MoveLocation(169, 77, CurrentPlayer, "Anywhere");
+      LMove(169, -x, -y);
+      MoveUnit(1, "40 + 1n Mojo", CurrentPlayer, 40, 169);
+      MoveUnit(1, "Protoss Dark Archon", CurrentPlayer, 40, 169);
+      KillUnitAt(All, "Protoss Dark Archon", "Anywhere", CurrentPlayer);
+      MoveLocation(169, 77, CurrentPlayer, "Anywhere");
+      SetDeaths(CurrentPlayer, Add, 1, " `SkillLoop");
+   },
+}
+
+x = 32;
+y = 32;
+
+Trigger { -- Skill : S
+   players = {Force1, Force2},
+   conditions = {
+      Deaths(CurrentPlayer, Exactly, 4000, 77);
+      Deaths(CurrentPlayer, Exactly, 100, " `SkillStep");
+      Bring(CurrentPlayer, AtLeast, 1, 77, "Anywhere");
+      Deaths(CurrentPlayer, Exactly, 1, " `SkillCount");
+      Deaths(CurrentPlayer, Exactly, 1, " `SkillLoop");
+   },
+   actions = {
+      Comment("Skill : S");
+      PreserveTrigger();
+      CreateUnit(2, "40 + 1n Mojo", "[Skill]Unit_Wait_1", CurrentPlayer);
+      CreateUnit(2, "Protoss Dark Archon", "[Skill]Unit_Wait_1", CurrentPlayer);
+      SetInvincibility(Enable, "Any unit", CurrentPlayer, 40);
+      MoveLocation(169, 77, CurrentPlayer, "Anywhere");
+      LMove(169, x, y);
+      MoveUnit(1, "40 + 1n Mojo", CurrentPlayer, 40, 169);
+      MoveUnit(1, "Protoss Dark Archon", CurrentPlayer, 40, 169);
+      MoveLocation(169, 77, CurrentPlayer, "Anywhere");
+      LMove(169, -x, -y);
+      MoveUnit(1, "40 + 1n Mojo", CurrentPlayer, 40, 169);
+      MoveUnit(1, "Protoss Dark Archon", CurrentPlayer, 40, 169);
+      KillUnitAt(All, "Protoss Dark Archon", "Anywhere", CurrentPlayer);
+      MoveLocation(169, 77, CurrentPlayer, "Anywhere");
+      SetDeaths(CurrentPlayer, Add, 1, " `SkillLoop");
+   },
+}
+
+
+x = -32;
+y = 32;
+
+Trigger { -- Skill : S
+   players = {Force1, Force2},
+   conditions = {
+      Deaths(CurrentPlayer, Exactly, 4000, 77);
+      Deaths(CurrentPlayer, Exactly, 100, " `SkillStep");
+      Bring(CurrentPlayer, AtLeast, 1, 77, "Anywhere");
+      Deaths(CurrentPlayer, Exactly, 1, " `SkillCount");
+      Deaths(CurrentPlayer, Exactly, 2, " `SkillLoop");
+   },
+   actions = {
+      Comment("Skill : S");
+      PreserveTrigger();
+      CreateUnit(2, "40 + 1n Mojo", "[Skill]Unit_Wait_1", CurrentPlayer);
+      CreateUnit(2, "Protoss Dark Archon", "[Skill]Unit_Wait_1", CurrentPlayer);
+      SetInvincibility(Enable, "Any unit", CurrentPlayer, 40);
+      MoveLocation(169, 77, CurrentPlayer, "Anywhere");
+      LMove(169, x, y);
+      MoveUnit(1, "40 + 1n Mojo", CurrentPlayer, 40, 169);
+      MoveUnit(1, "Protoss Dark Archon", CurrentPlayer, 40, 169);
+      MoveLocation(169, 77, CurrentPlayer, "Anywhere");
+      LMove(169, -x, -y);
+      MoveUnit(1, "40 + 1n Mojo", CurrentPlayer, 40, 169);
+      MoveUnit(1, "Protoss Dark Archon", CurrentPlayer, 40, 169);
+      KillUnitAt(All, "Protoss Dark Archon", "Anywhere", CurrentPlayer);
+      MoveLocation(169, 77, CurrentPlayer, "Anywhere");
+      SetDeaths(CurrentPlayer, Add, 1, " `SkillLoop");
+   },
+}
+
+x = -96;
+y = 32;
+
+Trigger { -- Skill : S
+   players = {Force1, Force2},
+   conditions = {
+      Deaths(CurrentPlayer, Exactly, 4000, 77);
+      Deaths(CurrentPlayer, Exactly, 100, " `SkillStep");
+      Bring(CurrentPlayer, AtLeast, 1, 77, "Anywhere");
+      Deaths(CurrentPlayer, Exactly, 1, " `SkillCount");
+      Deaths(CurrentPlayer, Exactly, 3, " `SkillLoop");
+   },
+   actions = {
+      Comment("Skill : S");
+      PreserveTrigger();
+      CreateUnit(2, "40 + 1n Mojo", "[Skill]Unit_Wait_1", CurrentPlayer);
+      CreateUnit(2, "Protoss Dark Archon", "[Skill]Unit_Wait_1", CurrentPlayer);
+      SetInvincibility(Enable, "Any unit", CurrentPlayer, 40);
+      MoveLocation(169, 77, CurrentPlayer, "Anywhere");
+      LMove(169, x, y);
+      MoveUnit(1, "40 + 1n Mojo", CurrentPlayer, 40, 169);
+      MoveUnit(1, "Protoss Dark Archon", CurrentPlayer, 40, 169);
+      MoveLocation(169, 77, CurrentPlayer, "Anywhere");
+      LMove(169, -x, -y);
+      MoveUnit(1, "40 + 1n Mojo", CurrentPlayer, 40, 169);
+      MoveUnit(1, "Protoss Dark Archon", CurrentPlayer, 40, 169);
+      KillUnitAt(All, "Protoss Dark Archon", "Anywhere", CurrentPlayer);
+      MoveLocation(169, 77, CurrentPlayer, "Anywhere");
+      Order("40 + 1n Mojo", CurrentPlayer, "Anywhere", Attack, 169);
+      Wait(0);
+      RemoveUnitAt(All, "40 + 1n Mojo", "Anywhere", CurrentPlayer);
+	  Wait(1000);
+      SetDeaths(CurrentPlayer, SetTo, 12, " `SkillWait");
+   },
+}
+
+Trigger { -- ===================================================
+	players = {Force1, Force2},
+	conditions = {
+		Never();
+	},
+	actions = {
+		Comment("===================================================");
+	},
+}
+
